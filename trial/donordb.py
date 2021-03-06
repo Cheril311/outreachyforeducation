4eeb9480c1bc4cfa3905e1f0830c3ffbf36e1051
Cheril311 <79891166+Cheril311@users.noreply.github.com>
2021-03-06 12:47:53 +0530

    Delete trial directory

--- trial/donordb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('donorinfo.db')
-print ("Opened database successfully")
-
-conn.execute('CREATE TABLE students (name TEXT, email TEXT, city TEXT, amount TEXT)')
-print ("Table created successfully")
-conn.close()
